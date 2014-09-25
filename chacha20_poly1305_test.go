44ee2a7c45468cc201e451cc624905f3e9cbcfcf
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2014-09-25 13:04:42 +0200

    Update documentation

--- chacha20_poly1305_test.go
+++ chacha20_poly1305_test.go
@@ -24,6 +24,7 @@ func TestAddBytesMod8FromUint64(t *testing.T) {
     assert.Equal(t, AddBytes(b, length, 8), []byte{0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0})
 }
 
+// Test how and if de-/encoding works with chacha20 - surprise it works
 func TestChacha20(t *testing.T) {
     K, _ := hex.DecodeString("6a3bfd77d9efac53f8ef51712796bf7a37541f425a5dc5397c8a2c3c040d9301")
     message, _ := hex.DecodeString("8e685bd3237866e7a424b0f33df1a087a397a78e147042d2d17b159044d2ad1162dea13df2a119b61c90d62fc76335f49954557f2b07c463dca1664ca042599fca66068b16bc3e7e1896536ca2")
