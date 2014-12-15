3b1a9c85b479587caa7af990308bfc2497c8590c
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2014-12-15 13:07:17 +0100

    Set outlet service type to outlet instead of switch

--- model/service/outlet.go
+++ model/service/outlet.go
@@ -13,7 +13,7 @@ func NewOutlet(name string, on, inUse bool) *Outlet {
     in_use := characteristic.NewInUse(on)
     
     sw := NewSwitch(name, on)
-    sw.Type = TypeSwitch
+    sw.Type = TypeOutlet
     sw.AddCharacteristic(in_use.Characteristic)
     
     return &Outlet{sw, in_use}
