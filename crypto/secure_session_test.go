92e122b4c4dc21558aaebca0d001533133567ee9
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2014-12-18 22:37:36 +0100

    Add test for max packet count

--- crypto/secure_session_test.go
+++ crypto/secure_session_test.go
@@ -5,6 +5,7 @@ import (
     "github.com/stretchr/testify/assert"
     "bytes"
     "io/ioutil"
+    "math"
 )
 
 func TestCrypto(t *testing.T) {
@@ -29,6 +30,33 @@ func TestCrypto(t *testing.T) {
     client.decryptCount = 128
     decrypted, err := client.Decrypt(encrypted)
     assert.Nil(t, err)
+    orig, err := ioutil.ReadAll(decrypted)
+    assert.Nil(t, err)
+    assert.Equal(t, orig, data)
+}
+
+func TestCryptoMaxPacketCount(t *testing.T) {
+    data := []byte{0x01, 0x02, 0x03}
+    key := [32]byte{
+        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 }
+    server, err := NewSecureSessionFromSharedKey(key)
+    client, err := NewSecureClientSessionFromSharedKey(key)
+    assert.Nil(t, err)
+    
+    var b bytes.Buffer
+    b.Write(data)
+    server.encryptCount = math.MaxUint64
+    encrypted, err := server.Encrypt(&b)
+    assert.Nil(t, err)
+    assert.Equal(t, server.encryptCount, uint64(0))
+
+    client.decryptCount = math.MaxUint64
+    decrypted, err := client.Decrypt(encrypted)
+    assert.Nil(t, err)
+    assert.Equal(t, client.decryptCount, uint64(0))
     orig, err := ioutil.ReadAll(decrypted)
     assert.Nil(t, err)
     assert.Equal(t, orig, data)
