314ed4c097a06ddadcc099c9cba4eded181c00c7
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2015-03-06 10:07:38 +0100

    Update documentation

--- netio/pair/tag_types.go
+++ netio/pair/tag_types.go
@@ -7,7 +7,7 @@ const (
 	TagUsername       = 0x01 // string
 	TagSalt           = 0x02 // 16 bytes
 	TagPublicKey      = 0x03 // either SRP client public key (384 bytes) or ED25519 LTPK (32 bytes)
-	TagProof          = 0x04 // 64 bytes (SRP proof)
+	TagProof          = 0x04 // SRP proof (64 bytes)
 	TagEncryptedData  = 0x05 // data with auth tag (mac)
 	TagSequence       = 0x06 // PairSequenceType
 	TagError          = 0x07 // ErrorType
