9c6818c7093d916d5d0104a4b32d9c2ac5f2b6b5
Erik Aigner <aigner.erik@gmail.com>
2015-05-06 21:26:27 +0200

    Fix TestEventCharacteristicFromJSON test

--- netio/data/characteristics_test.go
+++ netio/data/characteristics_test.go
@@ -2,19 +2,36 @@ package data
 
 import (
 	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestEventCharacteristicFromJSON(t *testing.T) {
 	var b = []byte(`{"characteristics":[{"aid":2,"iid":13,"ev":true}]}`)
-	var chars Characteristics
-	err := json.Unmarshal(b, &chars)
-	assert.Nil(t, err)
-	assert.Equal(t, len(chars.Characteristics), 1)
-	var char = chars.Characteristics[0]
-	assert.Equal(t, char.AccessoryID, 2)
-	assert.Equal(t, char.ID, 13)
-	assert.Equal(t, char.Events, true)
-	assert.Nil(t, char.Value)
+	var cs Characteristics
+
+	if err := json.Unmarshal(b, &cs); err != nil {
+		t.Fatal(err)
+	}
+	if x := len(cs.Characteristics); x != 1 {
+		t.Fatal(x)
+	}
+
+	c := cs.Characteristics[0]
+
+	if x := c.AccessoryID; x != 2 {
+		t.Fatal(x)
+	}
+	if x := c.ID; x != 13 {
+		t.Fatal(x)
+	}
+	if x, ok := c.Events.(bool); ok {
+		if !x {
+			t.Fatalf("want=true is=%v", x)
+		}
+	} else {
+		t.Fatal("invalid Events type")
+	}
+	if c.Value != nil {
+		t.Fatalf("want=nil is=%v", c.Value)
+	}
 }
