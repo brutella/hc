934912bba3c911e552e938203138097059a1cd29
Erik Aigner <aigner.erik@gmail.com>
2015-05-06 21:47:25 +0200

    Fix service package tests

--- model/service/service_test.go
+++ model/service/service_test.go
@@ -2,14 +2,17 @@ package service
 
 import (
 	"github.com/brutella/hc/model"
-
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestService(t *testing.T) {
 	s := New()
 
-	assert.Equal(t, s.GetID(), model.InvalidID)
-	assert.Equal(t, len(s.GetCharacteristics()), 0)
+	// TODO(brutella): int64 cast should not be required!
+	if is, want := s.GetID(), int64(model.InvalidID); is != want {
+		t.Fatalf("is=%v want=%v", is, want)
+	}
+	if is, want := len(s.GetCharacteristics()), 0; is != want {
+		t.Fatalf("is=%v want=%v", is, want)
+	}
 }
--- model/service/thermostat_test.go
+++ model/service/thermostat_test.go
@@ -2,28 +2,55 @@ package service
 
 import (
 	"github.com/brutella/hc/model"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestThermostat(t *testing.T) {
 	thermostat := NewThermostat("Testthermostat", 10.5, -10, 100, 1)
 
-	assert.Equal(t, thermostat.Type, typeThermostat)
-	assert.Equal(t, thermostat.Name.GetValue(), "Testthermostat")
-	assert.Equal(t, thermostat.Temp.GetValue(), 10.5)
-	assert.Equal(t, thermostat.TargetTemp.GetValue(), 10.5)
-	assert.Equal(t, thermostat.Mode.GetValue(), model.HeatCoolModeOff)
-	assert.Equal(t, thermostat.TargetMode.GetValue(), model.HeatCoolModeOff)
+	if is, want := thermostat.Type, typeThermostat; is != want {
+		t.Fatalf("type: is=%v want=%v", is, want)
+	}
+	if is, want := thermostat.Name.GetValue(), "Testthermostat"; is != want {
+		t.Fatalf("name: is=%v want=%v", is, want)
+	}
+	if is, want := thermostat.Temp.GetValue(), 10.5; is != want {
+		t.Fatalf("temp: is=%v want=%v", is, want)
+	}
+	if is, want := thermostat.TargetTemp.GetValue(), 10.5; is != want {
+		t.Fatalf("targettemp: is=%v want=%v", is, want)
+	}
+	// TODO(brutella): uint8 cast should not be required!
+	if is, want := thermostat.Mode.GetValue(), uint8(model.HeatCoolModeOff); is != want {
+		t.Fatalf("mode: is=%v want=%v", is, want)
+	}
+	// TODO(brutella): uint8 cast should not be required!
+	if is, want := thermostat.TargetMode.GetValue(), uint8(model.HeatCoolModeOff); is != want {
+		t.Fatalf("targetmode: is=%v want=%v", is, want)
+	}
 }
 
 func TestThermometer(t *testing.T) {
 	thermometer := NewThermometer("Thermometer", 10.5, -10, 100, 1)
 
-	assert.Equal(t, thermometer.Type, typeThermostat)
-	assert.Equal(t, thermometer.Name.GetValue(), "Thermometer")
-	assert.Equal(t, thermometer.Temp.GetValue(), 10.5)
-	assert.Equal(t, thermometer.TargetTemp.GetValue(), 10.5)
-	assert.Equal(t, thermometer.Mode.GetValue(), model.HeatCoolModeOff)
-	assert.Equal(t, thermometer.TargetMode.GetValue(), model.HeatCoolModeOff)
+	if is, want := thermometer.Type, typeThermostat; is != want {
+		t.Fatalf("type: is=%v want=%v", is, want)
+	}
+	if is, want := thermometer.Name.GetValue(), "Thermometer"; is != want {
+		t.Fatalf("name: is=%v want=%v", is, want)
+	}
+	if is, want := thermometer.Temp.GetValue(), 10.5; is != want {
+		t.Fatalf("temp: is=%v want=%v", is, want)
+	}
+	if is, want := thermometer.TargetTemp.GetValue(), 10.5; is != want {
+		t.Fatalf("targettemp: is=%v want=%v", is, want)
+	}
+	// TODO(brutella): cast should not be required!
+	if is, want := thermometer.Mode.GetValue(), uint8(model.HeatCoolModeOff); is != want {
+		t.Fatalf("mode: is=%v want=%v", is, want)
+	}
+	// TODO(brutella): cast should not be required!
+	if is, want := thermometer.TargetMode.GetValue(), uint8(model.HeatCoolModeOff); is != want {
+		t.Fatalf("targetmode: is=%v want=%v", is, want)
+	}
 }
