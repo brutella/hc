d793b8dc4044da4dd91c4b7c9d81aeb614361e8b
Erik Aigner <aigner.erik@gmail.com>
2015-05-06 16:59:04 +0200

    Remove testify dependency
    As an example for #12

--- crypto/packet_test.go
+++ crypto/packet_test.go
@@ -2,31 +2,38 @@ package crypto
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPacketFromBytes(t *testing.T) {
-	data := []byte{0x01, 0x02, 0x03}
-	var b bytes.Buffer
-	b.Write(data)
+	b := bytes.NewBuffer([]byte{0x01, 0x02, 0x03})
+	packets := packetsFromBytes(b)
 
-	packets := packetsFromBytes(&b)
-	assert.Equal(t, len(packets), 1)
+	if x := len(packets); x != 1 {
+		t.Fatal(x)
+	}
 
-	packet := packets[0]
-	assert.Equal(t, packet.length, 3)
-	assert.Equal(t, len(packet.value), packet.length)
+	p := packets[0]
+
+	if x := p.length; x != 3 {
+		t.Fatal(x)
+	}
+	if x := len(p.value); x != p.length {
+		t.Fatal(x)
+	}
 }
 
 func TestMultiplePacketFromBytes(t *testing.T) {
-	data := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
-	var b bytes.Buffer
-	b.Write(data)
+	b := bytes.NewBuffer([]byte{0x01, 0x02, 0x03, 0x04, 0x05})
+	p := packetsWithSizeFromBytes(3, b)
 
-	packets := packetsWithSizeFromBytes(3, &b)
-	assert.Equal(t, len(packets), 2)
-
-	assert.Equal(t, packets[0].length, 3)
-	assert.Equal(t, packets[1].length, 2)
+	if x := len(p); x != 2 {
+		t.Fatal(x)
+	}
+	if x := p[0].length; x != 3 {
+		t.Fatal(x)
+	}
+	if x := p[1].length; x != 2 {
+		t.Fatal(x)
+	}
 }
