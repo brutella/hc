673cf04bd5dad39ab79d43968685c8976e36a299
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2015-10-29 12:41:26 +0100

    Add missing files

---
+++ model/service/temperature_sensor.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"github.com/brutella/hc/model"
+	"github.com/brutella/hc/model/characteristic"
+)
+
+type TemperatureSensor struct {
+	*Service
+
+	Temp *characteristic.TemperatureCharacteristic
+	Name *characteristic.Name
+	Unit *characteristic.TemperatureUnit
+}
+
+// NewTemperatureSensor returns a temperature service service.
+func NewTemperatureSensor(name string, temperature, min, max, steps float64) *TemperatureSensor {
+	nameChar := characteristic.NewName(name)
+	tempUnit := model.TempUnitCelsius
+	unitChar := characteristic.NewTemperatureUnit(tempUnit)
+	temp := characteristic.NewCurrentTemperatureCharacteristic(temperature, min, max, steps, string(tempUnit))
+
+	svc := New()
+	svc.Type = typeTemperatureSensor
+	svc.AddCharacteristic(temp.Characteristic)
+	svc.AddCharacteristic(nameChar.Characteristic)
+	svc.AddCharacteristic(unitChar.Characteristic)
+
+	t := TemperatureSensor{svc, temp, nameChar, unitChar}
+
+	return &t
+}
