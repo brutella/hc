f09d4b1741f7f9a4343e130871cf1d5ddca24e98
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2016-03-10 12:33:09 +0100

    Add missing file

---
+++ model/accessory/constant.go
@@ -0,0 +1,21 @@
+package accessory
+
+type AccessoryType int
+
+const (
+	TypeOther              AccessoryType = 1
+	TypeBridge             AccessoryType = 2
+	TypeFan                AccessoryType = 3
+	TypeGarageDoorOpener   AccessoryType = 4
+	TypeLightBulb          AccessoryType = 5
+	TypeDoorLock           AccessoryType = 6
+	TypeOutlet             AccessoryType = 7
+	TypeSwitch             AccessoryType = 8
+	TypeThermostat         AccessoryType = 9
+	TypeSensor             AccessoryType = 10
+	TypeAlarmSystem        AccessoryType = 11
+	TypeDoor               AccessoryType = 12
+	TypeWindow             AccessoryType = 13
+	TypeWindowCovering     AccessoryType = 14
+	TypeProgrammableSwitch AccessoryType = 15
+)
