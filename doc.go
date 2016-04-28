ba2418d5fe058d9b568dda42039677b7a315d5a9
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2016-04-28 11:59:33 +0200

    Update go doc

--- doc.go
+++ doc.go
@@ -1,2 +1,13 @@
 // Package hc provides implementation of an IP transport for HomeKit accessories.
+//
+//     import (
+//         "github.com/brutella/hc"
+//         "github.com/brutella/hc/accessory"
+//     )
+//
+//     acc := accessory.NewSwitch(...)
+//     config := hc.Config{Pin: "00102003"}
+//     t, err := hc.NewIPTransport(config, acc.Accessory)
+//     ...
+//     t.Start()
 package hc
