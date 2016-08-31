2ab8971e568dea38ca05b5ce92ac2369f531b788
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2016-08-31 10:27:53 +0200

    Squashed 'vendor/github.com/tadglines/go-pkgs/' content from commit 1f86682
    git-subtree-dir: vendor/github.com/tadglines/go-pkgs
    git-subtree-split: 1f86682992f1a784aa7592b2be0ae80f194b2479

---
+++ crypto/srp/pbkdf2/pbkdf2.go
@@ -0,0 +1,29 @@
+// Copyright 2013 Tad Glines
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package pbkdf2
+
+import (
+	"code.google.com/p/go.crypto/pbkdf2"
+	"hash"
+)
+
+// NewPBKDF2 returns a new key derivation function that uses pbkdf2 to do
+// the derivation. The returned key size will be the same as the hash size.
+func NewPBKDF2(iter int, h func() hash.Hash) func(salt, password []byte) []byte {
+	return func(salt, password []byte) []byte {
+		hf := h()
+		return pbkdf2.Key(password, salt, iter, hf.Size(), h)
+	}
+}
