3f2bc468a227ee39522107629fee86d4c2b7c701
James Sanford <jsanfordgit@froop.com>
2017-11-22 22:04:05 -0800

    With explicit join in MDNSConn, clean up other uses of JoinGroup.

--- responder_debug.go
+++ responder_debug.go
@@ -2,18 +2,12 @@ package dnssd
 
 import (
 	"context"
-	"net"
 )
 
 type ReadFunc func(*Request)
 
 func (r *responder) Debug(ctx context.Context, fn ReadFunc) {
 	conn := r.conn.(*mdnsConn)
-	ifaces, _ := net.Interfaces()
-	for _, ifi := range ifaces {
-		conn.ipv4.JoinGroup(&ifi, &net.UDPAddr{IP: IPv4LinkLocalMulticast})
-		conn.ipv6.JoinGroup(&ifi, &net.UDPAddr{IP: IPv6LinkLocalMulticast})
-	}
 
 	readCtx, readCancel := context.WithCancel(ctx)
 	defer readCancel()
