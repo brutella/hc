805e4aef40191d35dabd229e903fabedb4eea316
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2018-02-10 21:38:47 +0100

    Don't allow non-admin client to add additional pairing

--- hap/pair/pairing_controller.go
+++ hap/pair/pairing_controller.go
@@ -7,16 +7,6 @@ import (
 	"github.com/brutella/hc/util"
 )
 
-// Pairing implements pairing json of format
-//     {
-//       "guestName": <string>,
-//       "guestPublicKey": <string>
-//     }
-type Pairing struct {
-	GuestName      string `json:"guestName"`
-	GuestPublicKey string `json:"guestPublicKey"`
-}
-
 // PairingController handles un-/pairing with a client by simply exchanging
 // the keys going through the pairing process.
 type PairingController struct {
@@ -35,20 +25,31 @@ func NewPairingController(database db.Database) *PairingController {
 // Handle processes a container to pair with a new client without going through the pairing process.
 func (c *PairingController) Handle(cont util.Container) (util.Container, error) {
 	method := PairMethodType(cont.GetByte(TagPairingMethod))
+	perm := cont.GetByte(TagPermission)
 	username := cont.GetString(TagUsername)
 	publicKey := cont.GetBytes(TagPublicKey)
 
-	log.Debug.Println("->   Method:", method)
-	log.Debug.Println("-> Username:", username)
-	log.Debug.Println("->     LTPK:", publicKey)
+	log.Debug.Println("->     Method:", method)
+	log.Debug.Println("-> Permission:", perm)
+	log.Debug.Println("->   Username:", username)
+	log.Debug.Println("->       LTPK:", publicKey)
 
 	entity := db.NewEntity(username, publicKey, nil)
+
+	out := util.NewTLV8Container()
+	out.SetByte(TagSequence, 0x2)
 
 	switch method {
 	case PairingMethodDelete:
 		log.Debug.Printf("Remove LTPK for client '%s'\n", username)
 		c.database.DeleteEntity(entity)
 	case PairingMethodAdd:
+		if perm != AdminPerm {
+			log.Info.Println("Non-admin controllers are not allowed to add pairings")
+			out.SetByte(TagErrCode, ErrCodeAuthenticationFailed.Byte())
+			break
+		}
+
 		err := c.database.SaveEntity(entity)
 		if err != nil {
 			log.Info.Panic(err)
@@ -57,9 +58,6 @@ func (c *PairingController) Handle(cont util.Container) (util.Container, error) 
 	default:
 		return nil, fmt.Errorf("Invalid pairing method type %v", method)
 	}
-
-	out := util.NewTLV8Container()
-	out.SetByte(TagSequence, 0x2)
 
 	return out, nil
 }
--- hap/pair/pairing_controller_test.go
+++ hap/pair/pairing_controller_test.go
@@ -11,6 +11,7 @@ import (
 func TestUnknownPairingMethod(t *testing.T) {
 	tlv8 := util.NewTLV8Container()
 	tlv8.SetByte(TagPairingMethod, 0x09)
+	tlv8.SetByte(TagPermission, AdminPerm)
 
 	database, _ := db.NewDatabase(os.TempDir())
 	controller := NewPairingController(database)
@@ -29,6 +30,7 @@ func TestAddPairing(t *testing.T) {
 	in := util.NewTLV8Container()
 	in.SetByte(TagPairingMethod, PairingMethodAdd.Byte())
 	in.SetByte(TagSequence, 0x01)
+	in.SetByte(TagPermission, AdminPerm)
 	in.SetString(TagUsername, "Unit Test")
 	in.SetBytes(TagPublicKey, []byte{0x01, 0x02})
 
@@ -43,6 +45,32 @@ func TestAddPairing(t *testing.T) {
 		t.Fatal("no response")
 	}
 	if is, want := out.GetByte(TagSequence), byte(0x2); is != want {
+		t.Fatalf("is=%v want=%v", is, want)
+	}
+}
+
+func TestAddPairingAsNonAdmin(t *testing.T) {
+	in := util.NewTLV8Container()
+	in.SetByte(TagPairingMethod, PairingMethodAdd.Byte())
+	in.SetByte(TagSequence, 0x01)
+	in.SetString(TagUsername, "Unit Test")
+	in.SetBytes(TagPublicKey, []byte{0x01, 0x02})
+
+	database, _ := db.NewDatabase(os.TempDir())
+	controller := NewPairingController(database)
+
+	out, err := controller.Handle(in)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if out == nil {
+		t.Fatal("no response")
+	}
+	if is, want := out.GetByte(TagSequence), byte(0x2); is != want {
+		t.Fatalf("is=%v want=%v", is, want)
+	}
+
+	if is, want := out.GetByte(TagErrCode), ErrCodeAuthenticationFailed.Byte(); is != want {
 		t.Fatalf("is=%v want=%v", is, want)
 	}
 }
