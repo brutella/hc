318d85029c6da78c5ad99342235a4c31a37ce220
mestafin <mestafin@mestafin.com>
2018-03-12 17:15:04 +0100

    add / update accessories: fan.go heater_cooler.go window_covering.go
    update service:
    heater_cooler - add optional TargetTemperature characteristic
    window_covering - add optional HoldPosition characteristic

---
+++ accessory/heater_cooler.go
@@ -0,0 +1,21 @@
+package accessory
+
+import (
+	"github.com/brutella/hc/service"
+)
+
+type HeaterCooler struct {
+	*Accessory
+
+	HeaterCooler *service.HeaterCooler
+}
+
+// NewSwitch returns a heater which implements model.Heater.
+func NewHeaterCooler(info Info) *HeaterCooler {
+	acc := HeaterCooler{}
+	acc.Accessory = New(info, TypeHeater)
+	acc.HeaterCooler = service.NewHeaterCooler()
+	acc.AddService(acc.HeaterCooler.Service)
+
+	return &acc
+}
